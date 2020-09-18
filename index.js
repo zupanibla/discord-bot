9c08fc36b6101bd77087dcfcb02f3d28d713083f
Blaž Zupančič <zupanibla@gmail.com>
2020-09-18 15:20:36 +0200

    Add 'listlatest' command

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const hound    = require('hound');
 
 // args
 if (process.argv.length != 4) {
-    console.log('Two positional arguments required: <bot token> <sound files path>');
+	console.log('Two positional arguments required: <bot token> <sound files path>');
 }
 const botToken       = process.argv[2];
 const soundFilesPath = process.argv[3];
@@ -28,98 +28,128 @@ const client = new (require('discord.js').Client)();
 
  // command message handler
 client.on('message', msg => {
-    if ( ['list', 'help', 'listcommands', 'sounds', 'listsounds'].includes(msg.content) ) {
-        // list sounds contained in the sound files directory (possibly in multiple messages)
-        // @TODO check how this behaves with duplicates (e.g. a.mp3, a.ogg)
-        let soundList = fs.readdirSync(soundFilesPath).map(v => v.split('.')[0]).join(', ');
-        let soundListChunks = soundList.match(/(.{1,1900}(\s|$))\s*/g);
-        for (let it of soundListChunks) {
-            msg.reply(it);
-        }
-    }
-    
-    if ( ['stop', 'skip'].includes(msg.content) ) {
-        // stop audio playback
-        if (msg.guild && msg.guild.me && msg.guild.me.voice &&
-            msg.guild.me.voice.connection && msg.guild.me.voice.connection.dispatcher) {
-            console.log("stopping");
-            msg.guild.me.voice.connection.dispatcher.pause();
-        }
-    }
+	if ( ['list', 'help', 'listcommands', 'sounds', 'listsounds'].includes(msg.content) ) {
+		// list sounds contained in the sound files directory (possibly in multiple messages)
+		// TODO check how this behaves with duplicates (e.g. a.mp3, a.ogg)
+		let soundList = fs.readdirSync(soundFilesPath).map(v => v.split('.')[0]).join(', ');
+		let soundListChunks = soundList.match(/(.{1,1900}(\s|$))\s*/g);
+		for (let it of soundListChunks) {
+			msg.reply(it);
+		}
+	}
+	
+	if ( ['stop', 'skip'].includes(msg.content) ) {
+		// stop audio playback
+		if (msg.guild && msg.guild.me && msg.guild.me.voice &&
+			msg.guild.me.voice.connection && msg.guild.me.voice.connection.dispatcher) {
+			console.log("stopping");
+			msg.guild.me.voice.connection.dispatcher.pause();
+		}
+	}
 
-    if ( ['bot pejt stran', 'bot odstran se', 'dc', 'leave'].includes(msg.content) ) {
-        // leave voice channel
-        if (msg.guild && msg.guild.me && msg.guild.me.voice.channel) {
-            msg.guild.me.voice.channel.leave();
-        }
-    }
+	if ( ['bot pejt stran', 'bot odstran se', 'dc', 'leave'].includes(msg.content) ) {
+		// leave voice channel
+		if (msg.guild && msg.guild.me && msg.guild.me.voice.channel) {
+			msg.guild.me.voice.channel.leave();
+		}
+	}
 
-    if ( ['leavelast'].includes(msg.content) ) {
-        // leave lastVoiceChannel
-        if (lastVoiceChannel) {
-            lastVoiceChannel.leave();
-        }
-    }
+	if ( ['leavelast'].includes(msg.content) ) {
+		// leave lastVoiceChannel
+		if (lastVoiceChannel) {
+			lastVoiceChannel.leave();
+		}
+	}
 
-    if ( msg.content.startsWith('echo ') ) {
-        // send a message to lastVoiceChannel
-        if (lastVoiceChannel) {
-            lastTextChannel.send(msg.content.substring('echo '.length)).catch(console.error);
-        }
-    }
+	if ( msg.content.startsWith('echo ') ) {
+		// send a message to lastVoiceChannel
+		if (lastVoiceChannel) {
+			lastTextChannel.send(msg.content.substring('echo '.length)).catch(console.error);
+		}
+	}
+
+	if (['listlatest', 'list latest'].includes(msg.content)) {
+		// replies with 10 latest files from 'soundFilesPath' and their creation dates
+		// TODO creation date vs added to bot date
+		let sortedFileList =
+			fs.readdirSync(soundFilesPath)
+			.map((it) => 
+				({
+					name: it,
+					time: new Date(fs.statSync(soundFilesPath + '/' + it).mtime.getTime()),
+				})
+			)
+			.sort( (a, b) => b.time - a.time );
+		
+		[].slice()
+		msg.reply(
+            '\n' +
+			sortedFileList
+            .slice(0, 10)
+			.map(it =>
+                it.time.getFullYear() + '/' +
+                String(it.time.getMonth() + 1).padStart(2, '0') + '/' +
+                String(it.time.getDate()     ).padStart(2, '0') + ' ' +
+                String(it.time.getHours()    ).padStart(2, ' ') + ':' +
+                String(it.time.getMinutes()  ).padStart(2, '0') + ' ' +
+                it.name
+            )
+			.join('\n')
+		);
+	}
 });
 
 
 function playSound(voiceChannel, soundFilePath) {
-    console.log('attempting to play ' + soundFilePath);
-    voiceChannel.join()
-      .then(con => {
-          const dispatcher = con.play(soundFilePath);
-          dispatcher.on('start', _ => console.log('started playing'));
-          dispatcher.on('error', err => console.log);
-      })
-      .catch(console.error);
+	console.log('attempting to play ' + soundFilePath);
+	voiceChannel.join()
+	  .then(con => {
+		  const dispatcher = con.play(soundFilePath);
+		  dispatcher.on('start', _ => console.log('started playing'));
+		  dispatcher.on('error', err => console.log);
+	  })
+	  .catch(console.error);
 }
 
 
  // soundboard message handler
 client.on('message', msg => {
-    // to play a sound we need a voice channel
-    // first try using users voice channel, then fall back to last used voice channel else return
-    let voiceChannel;
-    if (msg.member && msg.member.voice.channel) {
-        voiceChannel = msg.member.voice.channel;
-    } else if (lastVoiceChannel) {
-        voiceChannel = lastVoiceChannel;
-    } else {
-        return;
-    }
+	// to play a sound we need a voice channel
+	// first try using users voice channel, then fall back to last used voice channel else return
+	let voiceChannel;
+	if (msg.member && msg.member.voice.channel) {
+		voiceChannel = msg.member.voice.channel;
+	} else if (lastVoiceChannel) {
+		voiceChannel = lastVoiceChannel;
+	} else {
+		return;
+	}
 
-    // attempt to fetch soundName from message (to lowercase, remove non alphanumeric)
-    let soundName = msg.content.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '');
-    if (!soundName) return;
+	// attempt to fetch soundName from message (to lowercase, remove non alphanumeric)
+	let soundName = msg.content.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '');
+	if (!soundName) return;
 
-    // attempt to find the corresponding sound file
-    let soundFilePath = null;
-    for (it of [soundName, soundName + '.ogg', soundName + '.mp3']) {
-        soundFilePathCandidate = path.join(soundFilesPath, it);
-        if (fs.existsSync(soundFilePathCandidate)) {
-            soundFilePath = soundFilePathCandidate;
-            break;
-        }
-    }
+	// attempt to find the corresponding sound file
+	let soundFilePath = null;
+	for (it of [soundName, soundName + '.ogg', soundName + '.mp3']) {
+		soundFilePathCandidate = path.join(soundFilesPath, it);
+		if (fs.existsSync(soundFilePathCandidate)) {
+			soundFilePath = soundFilePathCandidate;
+			break;
+		}
+	}
 
-    // if a sound file was found ...
-    if (soundFilePath) {
-        // record used voice and text channel for notifications
-        lastVoiceChannel = voiceChannel;
-        if (msg.channel) {
-            lastTextChannel = msg.channel;
-        }
+	// if a sound file was found ...
+	if (soundFilePath) {
+		// record used voice and text channel for notifications
+		lastVoiceChannel = voiceChannel;
+		if (msg.channel) {
+			lastTextChannel = msg.channel;
+		}
 
-        // play the sound
-        playSound(voiceChannel, soundFilePath);
-    }
+		// play the sound
+		playSound(voiceChannel, soundFilePath);
+	}
 });
 
 
@@ -128,17 +158,17 @@ const NEW_SOUND_NOTIFICATION_SOUND = 'hereyougo.ogg';
 
 let watcher = hound.watch(soundFilesPath);
 watcher.on('create', (filePath, stats) => {
-    console.log(filePath, 'created!');
-    if (lastVoiceChannel && lastTextChannel) {
-        let fileName = filePath.split('/').pop();
-        playSound(lastVoiceChannel, path.join(soundFilesPath, NEW_SOUND_NOTIFICATION_SOUND));
-        lastTextChannel.send(`Your precious ${fileName}, gratefully accepted! We will need it.`).catch(console.error);
-    }
+	console.log(filePath, 'created!');
+	if (lastVoiceChannel && lastTextChannel) {
+		let fileName = filePath.split('/').pop();
+		playSound(lastVoiceChannel, path.join(soundFilesPath, NEW_SOUND_NOTIFICATION_SOUND));
+		lastTextChannel.send(`Your precious ${fileName}, gratefully accepted! We will need it.`).catch(console.error);
+	}
 });
 
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 
